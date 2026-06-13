2c328720f289af7abc876f5cd8a3121a78591641
Aman Kumar <kumar.ak16121999@gmail.com>
2026-06-13 02:01:42 +0200

    chore: refactored to JobRetryResponse DTO for job retry logic

--- job-worker-service/src/main/java/com/aman/asyncjob/worker/controller/JobRetryController.java
+++ job-worker-service/src/main/java/com/aman/asyncjob/worker/controller/JobRetryController.java
@@ -1,5 +1,6 @@
 package com.aman.asyncjob.worker.controller;
 
+import com.aman.asyncjob.worker.dto.JobRetryResponse;
 import com.aman.asyncjob.worker.service.JobRetryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +15,12 @@ public class JobRetryController {
 
     private final JobRetryService jobRetryService;
 
-    //ToDo: implement this properly with DTO response once happy path works fine.
     @PostMapping("/{jobId}/retry")
-    public ResponseEntity<String> retryDeadJob(@PathVariable String jobId) {
-        String response = jobRetryService.requeueDeadJob(jobId);
-        if (response.contains("success")) {
+    public ResponseEntity<JobRetryResponse> retryDeadJob(@PathVariable String jobId) {
+        var response = jobRetryService.requeueDeadJob(jobId);
+        if (response.isRetried()) {
             return ResponseEntity.ok(response);
         }
-        return ResponseEntity.badRequest()
-                .body(response);
+        return ResponseEntity.badRequest().body(response);
     }
 }
